97da398772122ab341d5ea30b46b9b8c165ac039
jMujunen <jMujunen@github.com>
2024-05-07 23:12:55 -0700

    Finished dos2unix recursive feature

--- dos2unix.sh
+++ dos2unix.sh
@@ -3,13 +3,19 @@
 # dos2unix_recursive.sh - Recursively converts all files in the current directory from DOS format to UNIX format
 
 # Print help message
-if [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
-    echo "Usage: dos2unix_recursive.sh [-h|--help]"
+print_help() {
+    echo "Converts all files in the provided directory from DOS to UNIX line endings."
+    echo "Usage: dos2unix.sh [-h|--help] DIRECTORY"
+}
+if [ "$1" == "-h" ] || [ "$1" == "--help" ] || [ "$#" -gt 1 ]; then
+    print_help
     exit 0
 fi
 
-
-# Recursively convert all files in the current directory from DOS format to UNIX format
-for file in *; do
-    dos2unix "$file"
-done
+if [ "$#" -eq 0 ]; then
+    DIR="$(pwd)"
+else
+    DIR="$1"
+fi
+# Convert dos line endings to unix
+find $DIR -type f -name "*.[pPsS][yYhH]" -print0 | xargs -0 dos2unix
