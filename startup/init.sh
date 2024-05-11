04489ebb4fbabf323d53861a27a57d4bc9de7bab
jMujunen <jMujunen@github.com>
2024-05-11 06:36:52 -0700

    Modified init script to redirect stderr only when running corectl ------------------------------------------------------------------
    Previously, both stdout and stderr were being redirected to `/dev/null`,
    but now only stderr is being redirected while stdout is being sent to the null device.
    This change allows for any output for debugging purposes if needed.
    
    - Redirected stderr to `/dev/null` for corectl in init.sh script

--- startup/init.sh
+++ startup/init.sh
@@ -5,4 +5,4 @@
 sleep 5
 /bin/python3 /home/joona/python/Projects/Logging/csv_output.py
 sleep 5
-nohup /bin/corectrl -m PowerSave --minimize-systray 2>/dev/null 1>2&
+nohup /bin/corectrl -m PowerSave --minimize-systray >/dev/null 2>&1
