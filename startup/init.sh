028d9fe88a4380ff451a839783eba9550f26a019
jMujunen <jMujunen@github.com>
2024-05-28 17:00:22 -0700

    Add recovery script for Git repository
    - Added `recover.sh` script to recover each blob in the Git repository.
    - Implemented recursive tree recovery function.
    - Updated `init.sh` to include `recover_tree` command.

--- startup/init.sh
+++ startup/init.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 # tmp hwdata log files
-#python3 -u /home/joona/python/Projects/Logging/csv_output.py
-/bin/corectrl -m PowerSave --minimize-systray
-/usr/bin/ollama serve
+python3 -u /home/joona/python/Projects/Logging/csv_output.py
+#/bin/corectrl -m PowerSave --minimize-systray
+#/usr/bin/ollama serve
