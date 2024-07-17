37f99c57cde93dbb87cef0f751efa6111aa73468
jMujunen <jMujunen@github.com>
2024-07-17 03:30:52 -0700

    Refactor sync_obs.sh for new mount point
    - Changed mount point from /mn/removaable to /mn/win_ssd
    - Updated find command to search in new directory structure

--- screenshots/capture.sh
+++ screenshots/capture.sh
@@ -5,6 +5,7 @@ FILENAME="$HOME/Pictures/Screenshots/Screenshot_$(date +%F_%H-%M-%S).png"
 
 # Take a screenshot with scrot and save it to the defined filename
 scrot "$FILENAME"
+kitten icat "$FILENAME"
 
 # Notify the user where the screenshot has been saved
 notify-send -i camera-photo-symbolic -t 3000 "Screenshot Taken" "Screenshot saved as $FILENAME"
--- sync_obs.sh
+++ sync_obs.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-sudo mount /dev/sdb1 /mnt/removable
-
-for folder in $(find /mnt/removable/Users/Joona/Videos/NVIDIA/ -type d); do
+for folder in $(find /mnt/win_ssd/Users/Joona/Videos/NVIDIA/ -type d); do
     python3 /home/joona/python/scripts/batch_compress_mp4.py "$folder" \
     "/mnt/ssd/OBS/Joona/$folder/"
 done
