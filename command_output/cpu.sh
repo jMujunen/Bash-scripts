676dc17115fc3da7d540ffd2e050d22ac94b9171
jMujunen <jMujunen@github.com>
2024-10-30 10:55:57 -0700

    Update command output to reflect changes in /tmp/hwinfo.csv structure

--- command_output/cpu.sh
+++ command_output/cpu.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-line=$(tail -1 /tmp/hwinfo.csv | awk '{print $3" V,",$4" °C,",$5" MHz,",$6" MHz"}' | sed -E 's/([0-9]),/\1/g')
+line=$(tail -1 /tmp/hwinfo.csv | awk '{print $10" V,",$11" °C,",$12" MHz,",$13" MHz"}' | sed -E 's/([0-9]),/\1/g')
 
 echo $line
