65dfab60850033d1e790d70aea39337d0b104938
jMujunen <jMujunen@github.com>
2024-11-10 17:51:27 -0800

    Update command output scripts to reflect changes in upstream api

--- command_output/cpu.sh
+++ command_output/cpu.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
-line=$(tail -1 /tmp/hwinfo.csv | awk '{print $10" V,",$11" °C,",$12" MHz,",$13" MHz"}' | sed -E 's/([0-9]),/\1/g')
+                                        #"VOLTAGE | TEMP | CLOCK | MAX | CLOCK"
+line=$(tail -1 /tmp/hwinfo.csv | awk '{print $16" V,",$13" °C,",$14" MHz,",$15" MHz"}' | sed -E 's/([0-9]),/\1/g')
 
 echo $line
